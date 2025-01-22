ffa08370e33212941264afa3a7bd0f3aa5d8dfc6
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2025-01-22 03:28:12 +0200

    Fix test/models/template_test.rb

--- test/models/template_test.rb
+++ test/models/template_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 class TemplateTest < ActiveSupport::TestCase
   test "destroying template nullifies generator_option template_id" do
-    template = templates(:one) # Assuming you have a fixture for templates
-    generator_option = generator_options(:one) # Assuming you have a fixture for generator_options
+    template = templates(:minimal_template) # Assuming you have a fixture for templates
+    generator_option = generator_options(:generator_options_minimal) # Assuming you have a fixture for generator_options
 
     assert_equal template.id, generator_option.template_id # Ensure the association exists
     template.destroy!
